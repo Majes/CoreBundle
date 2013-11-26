90aa68aac556cc4118bf665a2c2c564b8d0d1900
Guillaume <gbeauvoir@majes.fr>
2013-11-26 14:06:24 +0100

    improvement

--- Services/Notification.php
+++ Services/Notification.php
@@ -7,38 +7,40 @@ use Symfony\Component\HttpFoundation\Session\Session;
 class Notification
 {
 
-	private $_session;
-	private $_source = null;
-	private $_notices;
-	private $_messages;
+    private $_session;
+    private $_source = null;
+    private $_notices;
+    private $_messages;
 
     public function __construct(Session $session, $source)
     {
-    	$this->_session = $session;
-    	$this->_source = $source;
+        $this->_session = $session;
+        $this->_source = $source;
 
-    	if(!$this->_session->has('notices'))
-    		$this->_session->set('notices', array());
+        if(!$this->_session->has('notices'))
+            $this->_session->set('notices', array());
 
-    	if(!$this->_session->has('messages'))
-    		$this->_session->set('messages', array());
+        if(!$this->_session->has('messages'))
+            $this->_session->set('messages', array());
 
-    	$this->_notices = $this->_session->get('notices');
-    	$this->_messages = $this->_session->get('messages');
+        $this->_notices = $this->_session->get('notices');
+        $this->_messages = $this->_session->get('messages');
     }
 
 
     public function reinit(){
 
-    	$this->_notices[$this->_source] = array();
-		$this->_messages[$this->_source] = array();
+        $this->_notices[$this->_source] = array();
+        $this->_messages[$this->_source] = array();
+
+        $this->setSession();
 
     }
 
     public function set(Array $arguments){
-    	foreach($arguments as $key => $value)
-    		if(isset($this->$key))
-    			$this->$key = $value;
+        foreach($arguments as $key => $value)
+            if(isset($this->$key))
+                $this->$key = $value;
 
     }
     
@@ -47,50 +49,50 @@ class Notification
      */
     public function add($type, $data){
 
-    	if(is_null($this->_source))
-    		return false;
+        if(is_null($this->_source))
+            return false;
 
-    	switch($type){
-    		case 'notices':
-    			$this->_notices[$this->_source][] = $data;
-    			break;
-    		case 'messages':
-    			$this->_messages[$this->_source][] = $data;
-    			break;
+        switch($type){
+            case 'notices':
+                $this->_notices[$this->_source][] = $data;
+                break;
+            case 'messages':
+                $this->_messages[$this->_source][] = $data;
+                break;
 
-    		default:
-    			break;
-    	}
+            default:
+                break;
+        }
 
-    	$this->setSession();
+        $this->setSession();
 
     }
 
     public function count($type){
 
-    	switch($type){
-    		case 'notices':
-    			$notification = $this->_notices;
-    			break;
-    		case 'messages':
-    			$notification = $this->_messages;
-    			break;
+        switch($type){
+            case 'notices':
+                $notification = $this->_notices;
+                break;
+            case 'messages':
+                $notification = $this->_messages;
+                break;
 
-    		default:
-    			break;
-    	}
+            default:
+                break;
+        }
 
-    	$count = 0;
-    	foreach ($notification as $key => $source) {
-    		$count += count($source);
-    	}
-    	return $count;
+        $count = 0;
+        foreach ($notification as $key => $source) {
+            $count += count($source);
+        }
+        return $count;
 
     }
 
     private function setSession(){
-    	$this->_session->set('notices', $this->_notices);
-    	$this->_session->set('messages', $this->_messages);
+        $this->_session->set('notices', $this->_notices);
+        $this->_session->set('messages', $this->_messages);
     }
 
 }
