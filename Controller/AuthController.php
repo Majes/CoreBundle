1fb22177cda01744a3ae59662c15854baefa08c9
BabarMajes <al@neomajes.com>
2014-10-28 15:09:04 +0100

    Never gonna give you up

--- Controller/AuthController.php
+++ Controller/AuthController.php
@@ -37,4 +37,52 @@ class AuthController extends Controller implements SystemController
          */
         return $this->render('MajesCoreBundle:Auth:login.html.twig');
     }
+
+    public function forgotAction()
+    {
+        /*
+         * The action's view can be rendered using render() method
+         * or @Template annotation as demonstrated in DemoController.
+         *
+         */
+
+        $request = $this->getRequest();
+
+        $em = $this->getDoctrine()->getManager();
+    
+        if($request->getMethod() == 'POST'){
+            $usertochange = $em->getRepository('MajesCoreBundle:User\User')->findOneByUsername($request->request->get('_username'));
+            if(is_object($usertochange)){
+                $characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+                $newpassword = '';
+                for ($i = 0; $i < 8; $i++) {
+                    $newpassword .= $characters[rand(0, strlen($characters) - 1)];
+                }
+
+                $factory = $this->container->get('security.encoder_factory');
+                $encoder = $factory->getEncoder($usertochange);
+                $pwd = $encoder->encodePassword($newpassword, $usertochange->getSalt());
+
+                $usertochange->setPassword($pwd);
+
+                $em->flush();
+
+                $transport = \Swift_MailTransport::newInstance();
+
+                $mailer = \Swift_Mailer::newInstance($transport);
+
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Reinitialisation du mot de passe')
+                    ->setFrom(array('dev@neomajes.com' => 'Neomajes'))
+                    ->setTo($usertochange->getEmail())
+                    ->setBody('Bonjour,<br/><br/><strong>Vous avez oublié ou perdu votre mot de passe pour accéder ?</strong><br/>Votre nouveau mot de passe: '.$newpassword.'<br/><br/>Cordialement,<br/><br/>Neomajes', 'text/html');
+
+                $result=$mailer->send($message);
+            }
+
+            return $this->redirect($this->get('router')->generate('_admin_login'));
+        }
+
+        return $this->render('MajesCoreBundle:Auth:forgot.html.twig', array('auth' => true));
+    }
 }
