9ffba00f432709618232e8bf8f3795afbc925f6f
BabarMajes <al@neomajes.com>
2015-07-06 11:41:12 +0200

    Fixing Google Analytics

--- Services/GoogleAnalytics.php
+++ Services/GoogleAnalytics.php
@@ -61,7 +61,7 @@ class GoogleAnalytics{
 		$response = $client->setAssertionCredentials($cred);
 
 
-		if(!empty($stats) || is_null($response)){
+		if(!empty($stats) || is_null($cred) || is_null($analytics) || is_null($client)){
 			return;
 		}
 		
