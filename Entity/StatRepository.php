6359a44b81314995057d35ae48fe24b57ac6830a
Guillaume de Beauvoir <gbeauvoir@majes.fr>
2015-07-08 12:00:50 +0200

    stats to 6 months

--- Entity/StatRepository.php
+++ Entity/StatRepository.php
@@ -1,21 +1,21 @@
-<?php 
+<?php
 namespace Majes\CoreBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
 
 class StatRepository extends EntityRepository {
- 
+
    	public function pastMonth(){
 
-   		$begin = new \DateTime(date('Y-m-01'));
-   		$begin3M = $begin->sub(new \DateInterval('P3M'));
+        $begin = new \DateTime(date('Y-m-01'));
+   		$begin6M = $begin->sub(new \DateInterval('P6M'));
 
    		$query = $this->createQueryBuilder('s')
             ->where('s.beginDate >= :begin')
             ->andWhere('s.current = 1')
-            ->setParameter('begin', $begin3M)
+            ->setParameter('begin', $begin6M)
             ->getQuery();
 
         return $results = $query->getResult();
-   	} 
+   	}
 }
